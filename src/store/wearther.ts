ae165b90b1d51630b2f0676bed022c2f666ff505
奕-Sugon <yihao168830@163.com>
2022-10-12 20:57:39 +0800

    ✨ feat(store): 新增组件间通信mobx

---
+++ src/store/wearther.ts
@@ -0,0 +1,28 @@
+import yhHttp from "../http";
+import { makeAutoObservable } from 'mobx'
+
+
+
+const instance = yhHttp.getInstance()
+
+
+class WeartherStore {
+
+    weartherList = []
+    
+    constructor() {
+        makeAutoObservable(this)
+    } 
+
+    getWeartheList = () => {
+        instance.get('/api/tianqi?api_key=9352759393aef152&city=天津&type=2').then((res) => {
+            console.log(res);
+            this.weartherList = res.data
+        })
+    }
+    
+    
+}
+
+
+export default WeartherStore

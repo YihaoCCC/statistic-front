b2c005e696e7e0bbeeff465254c4d3649915a6b4
YihaoCCC <yihao168830@163.com>
2022-10-13 20:10:58 +0800

    🌈 style(天气): 增加颜色

--- src/pages/utils/WeatherIconCom.tsx
+++ src/pages/utils/WeatherIconCom.tsx
@@ -5,18 +5,26 @@ export default function WeatherIconCom (props:MyWeather) {
     let iconColor
     if(weatherName === '晴') {
         iconName = 'ri-sun-line'
+        iconColor = '#fdd835'
     } else if (weatherName === '多云转阴') {
         // 多云转阴
         iconName = "ri-cloudy-line"
+        iconColor = '#42a5f5'
     } else if(weatherName === '多云') {
         // 多云
         iconName = 'ri-sun-cloudy-line'
+        iconColor = '#42a5f5'
     } else if(weatherName === '阴转晴') {
         // 阴转晴
         iconName = 'ri-sun-foggy-line'
+        iconColor = '#fdd835'
+    } else if(weatherName === '阴') {
+        iconName = 'ri-foggy-fill'
+        iconColor = '#fff'
     } else {
         // 雨天
         iconName = 'ri-drizzle-line'
+        iconColor = '#fdd835'
     }   
     return (
         <>
